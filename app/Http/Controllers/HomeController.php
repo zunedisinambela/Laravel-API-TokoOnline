a11aa78bf29d1dee7bc1541b7d33f9c3cc084359
zunedisinambela <zunedisinambela@gmail.com>
2023-01-03 16:09:26 +0700

    Dashboard Notification Count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailsTransaction;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard');
+        $transaction = Transaction::whereMonth('created_at', Carbon::now()->format('m'))->count();
+        $productSale = DetailsTransaction::whereMonth('created_at', Carbon::now()->format('m'))->sum('qty');
+
+        $customer = User::where('is_admin', false)->count();
+        $products = Product::count();
+
+        // dd($transaction, $productSale, $customer, $products);
+
+        return view('admin.dashboard', compact('customer', 'products', 'transaction', 'productSale'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Transaction;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\UsersController;
 use App\Http\Controllers\Web\ProductController;
