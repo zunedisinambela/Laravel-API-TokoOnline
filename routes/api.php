6ba3cbae535babbe00b4e731c954f30a06166d1c
zunedisinambela <zunedisinambela@gmail.com>
2023-01-05 20:57:53 +0700

    Response Pertama

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('halo', function(){
+
+    $biodata = [
+        'name' => 'Arjun Sinambela',
+        'email' => 'zunedisinambela@gmail.com',
+        'gender' => 'male',
+        'mediasosial' => [
+            [
+                'facebook' => 'www.facebook.com/zunedisinambela',
+                'instagram' => 'www.instagram.com/arjun.sinambela'
+            ]
+        ]
+    ];
+
+    return \Response::json($biodata);
+});
