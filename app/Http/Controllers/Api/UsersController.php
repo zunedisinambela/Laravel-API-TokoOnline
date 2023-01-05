48ac40adae7d895efa58ce88de7eb62f559688dc
zunedisinambela <zunedisinambela@gmail.com>
2023-01-06 06:46:06 +0700

    API Login

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Auth;
 use Response;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,5 +17,28 @@ class UsersController extends Controller
 
         // return Response::json($users);
         return UsersResource::collection($users);
+    }
+
+    public function login(Request $request)
+    {
+        // return $request->all();
+
+        if ( ! Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return Response::json([
+                'status' => [
+                    'code' => 401,
+                    'description' => 'Credential is wrong',
+                ]
+            ], 401);
+        }
+
+        $loginUser = Auth::user();
+
+        return (new UsersResource($loginUser))->additional([
+            'status' => [
+                'code' => 202,
+                'description' => 'Accepted',
+            ]
+        ])->response()->setStatusCode(202);
     }
 }
